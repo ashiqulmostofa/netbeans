2c7aaa85bb4ae97837c3627fcee339cb0f0e47ca
ashiqul mostofa <dugdugy@gmail.com>
2011-02-08 02:27:59 +0600

    hello

---
+++ hello/src/hello/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hello;
+import java.util.*;
+/**
+ *
+ * @author sid
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+Scanner in = new Scanner (System.in);
+        int u;
+u=in.nextInt();
+
+        System.out.print("hello world");
+        u=98;
+        // TODO code application logic here
+    }
+
+}
